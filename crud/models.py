b329c0e4820798b82b10d3b3be6ab12fb25af2e2
Daniel <adeboyed93@gmail.com>
2022-10-27 08:15:37 -0700

    added models and views

--- crud/models.py
+++ crud/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+class task(models.Model):
+  task_name = models.CharField(max_length=500)
+  is_completed = models.BooleanField(default=False)
+
+  def __str__(self):
+    return self.name
